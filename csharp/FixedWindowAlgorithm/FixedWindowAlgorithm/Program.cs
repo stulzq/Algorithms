8b914facfdc1397a9a37a662ab7c9c88c67bde79
stulzq <stulzq@qq.com>
2022-02-17 13:49:03 +0800

    fixed window

---
+++ csharp/FixedWindowAlgorithm/FixedWindowAlgorithm/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FixedWindowAlgorithm
+{
+    class Program
+    {
+        private static long _currentTime;
+        private static long _current;
+        private static Semaphore _semaphore = new Semaphore(0, 10);
+        static void Main(string[] args)
+        {
+            _currentTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+            _current = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 10000; j++)
+                    {
+                        FixedWindow();
+                    }
+
+                    _semaphore.Release(1);
+                });
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                _semaphore.WaitOne();
+            }
+            
+            Console.WriteLine(_current);
+        }
+
+        static long FixedWindow()
+        {
+            var now = DateTimeOffset.Now.ToUnixTimeSeconds();
+            var ct = Interlocked.Read(ref _currentTime);
+            if (now > ct)
+            {
+                if (Interlocked.CompareExchange(ref _currentTime, now, ct)==ct)
+                {
+                    return Interlocked.Exchange(ref _current, 0);
+                }
+            }
+            
+            return Interlocked.Increment(ref _current);
+        }
+    }
+}
